d4b95d0565aa4c3dcdafd9456b0606356e20697f
Moinak Ghosh <ghoshjimoinak@gmail.com>
2025-09-10 14:46:37 +0530

    First Commit - pushed project

---
+++ script.js
@@ -0,0 +1,12 @@
+function addRow() {
+  const table = document.getElementById("studentTable").getElementsByTagName("tbody")[0];
+  const newRow = table.insertRow();
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  const cell3 = newRow.insertCell(2);
+
+  cell1.innerHTML = "3";
+  cell2.innerHTML = "Kabir";
+  cell3.innerHTML = "Physics";
+}
